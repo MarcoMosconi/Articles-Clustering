c05a2dd1ed678daff8f6451535270abb1f639215
MarcoMosconi <marcom.mosconi@gmail.com>
2024-09-21 12:20:12 +0200

    First Commit

---
+++ scrape.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+def scrape(article_content, all_articles):
+    link = article_content.find("a", href=True)
+    title = link.text
+    soup2 = BeautifulSoup(requests.get(f"https://www.ansa.it{link['href']}").content, 'lxml')
+
+    full_text = soup2.find('div', class_='post-single-text rich-text news-txt')
+    text = ' '.join([p.text for p in full_text.find_all('p') if 'Riproduzione riservata © Copyright ANSA' not in p.text])
+
+    all_articles.append({
+        'Publication': 'Ansa',
+        'Title': title,
+        'Content': text
+    })
+    return all_articles
